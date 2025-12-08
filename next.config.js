791a885cba42e2872582c7b0280e67466ff3a864
joesefx22 <joesefmohamed98@gmail.com>
2025-12-08 21:37:44 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: { appDir: true },
+  headers: async () => {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          { key: 'X-Content-Type-Options', value: 'nosniff' },
+          { key: 'X-Frame-Options', value: 'SAMEORIGIN' },
+          { key: 'Referrer-Policy', value: 'no-referrer' }
+        ]
+      }
+    ]
+  }
+}
+
+module.exports = nextConfig
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
   images: {
     domains: ['localhost'],
   },
