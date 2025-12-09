0bbb8f91291de051493a43c2615f125e86fc5499
joesefx22 <joesefmohamed98@gmail.com>
2025-12-09 11:38:54 +0200

    Update prisma.ts

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,3 +1,14 @@
+import { PrismaClient } from "@prisma/client";
+
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
+
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    log: ["query", "error"],
+  });
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 // lib/prisma.ts
 import { PrismaClient } from '@prisma/client'
 
